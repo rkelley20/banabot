df04330885006c84dc45a66b22ac89d1f7cc5a77
Ryan Kelley <ryan.kelley@ttu.edu>
2020-02-18 10:02:03 -0600

    Add general error reporting for the entire bot

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ from colorama import Fore, Style
 import config
 from os import listdir, getcwd
 from os.path import isfile, join
+import traceback
 
 
 class BanaContext(commands.Context):
@@ -18,20 +19,42 @@ class BanaContext(commands.Context):
         except discord.HTTPException:
             pass
 
+    
+    async def check(self):
+        self.tick()
+    
+    async def cross(self):
+        self.tick(False)
+
 
 class BanaBot(commands.Bot):
     async def get_context(self, message, *, cls=BanaContext):
         return await super().get_context(message, cls=cls)
+    
+    async def on_command_error(self, ctx, error):
+        await ctx.cross()
+        if isinstance(error, commands.NoPrivateMessage):
+            await ctx.author.send('This command cannot be used in private messages.')
+        elif isinstance(error, commands.DisabledCommand):
+            await ctx.author.send('Sorry. This command is disabled and cannot be used.')
+        elif isinstance(error, commands.CommandInvokeError):
+            original = error.original
+            if not isinstance(original, discord.HTTPException):
+                print(f'In {ctx.command.qualified_name}:', file=sys.stderr)
+                traceback.print_tb(original.__traceback__)
+                print(f'{original.__class__.__name__}: {original}', file=sys.stderr)
+        elif isinstance(error, commands.ArgumentParsingError):
+            await ctx.send(error)
 
 bot = BanaBot(command_prefix='$', description='BanaBot: A bot build on SQLite.')
 bot.cfg = config
 
 @bot.event
 async def on_ready():
-    print(f'\n{Fore.RED}--------------------{Style.RESET_ALL}')
+    print(f'\n{Fore.RED}------------------------------{Style.RESET_ALL}')
     print(f'{Fore.GREEN}Logged in as:{Style.RESET_ALL}')
     print(f'{Fore.CYAN}{bot.user.name} {Fore.YELLOW}-{Fore.CYAN} {bot.user.id}{Style.RESET_ALL}')
-    print(f'{Fore.RED}--------------------{Style.RESET_ALL}\n')
+    print(f'{Fore.RED}------------------------------{Style.RESET_ALL}\n')
 
 # Here we load our extensions(cogs) listed above in [initial_extensions].
 if __name__ == '__main__':
@@ -41,11 +64,12 @@ if __name__ == '__main__':
     cogs_folder = 'cogs'
 
     def valid_cog(fname: str):
-        return isfile(join(cogs_folder, fname)) and fname not in exclude and fname.endswith('.py')
+        return isfile(fname not in exclude and join(cogs_folder, fname)) and fname.endswith('.py')
 
     cogs = ['cogs.' + fname.rstrip('.py') for fname in listdir(cogs_folder) if valid_cog(fname)]
 
-    print(f'Loading the following cogs:\n {cogs}')
+    cogstr = '\n'.join(cogs)
+    print(f'\n{Fore.GREEN}Loading the following cogs:{Style.RESET_ALL}\n{Fore.YELLOW}{cogstr}{Style.RESET_ALL}')
 
     for cog in cogs:
         bot.load_extension(cog)
